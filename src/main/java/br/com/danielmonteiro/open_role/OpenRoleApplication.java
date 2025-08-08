79abfd2e4391d10e41fd634b50021b8154fea164
daniel <danielchsan@gmail.com>
2025-08-08 09:15:22 -0300

    Create a `Mavem Project` whith `Spring Initializer`

---
+++ src/main/java/br/com/danielmonteiro/open_role/OpenRoleApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielmonteiro.open_role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenRoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danielmonteiro/open_role/OpenRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielmonteiro.open_role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
