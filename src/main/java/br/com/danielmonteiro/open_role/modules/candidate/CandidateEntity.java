49aaf5163555d6e54a2da4e1263dca4ac6737d3d
daniel <danielchsan@gmail.com>
2025-08-15 18:33:03 -0300

    adicionando dependencia `validation` e adicionando validações na `CandidateEntity`

--- src/main/java/br/com/danielmonteiro/open_role/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/danielmonteiro/open_role/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.danielmonteiro.open_role.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[A-Za-z0-9._]{3,20}$", message = "Username must be between 3 and 20 characters long and can only contain letters, numbers, periods, and underscores")
     private String username;
+
+    @Email(message = "Please, insert a valid e-mail")
     private String email;
+
+    @Length(min = 6, max = 100)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/danielmonteiro/open_role/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/danielmonteiro/open_role/modules/candidate/controllers/CandidateController.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.danielmonteiro.open_role.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidate");
+        System.out.println(candidateEntity.getUsername());
         System.out.println(candidateEntity.getEmail());
     }
 }
