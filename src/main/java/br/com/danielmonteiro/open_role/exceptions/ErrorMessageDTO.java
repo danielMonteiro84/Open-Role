73bcb9ed4eb450f23e25925de9ba8cb11b4c56eb
daniel <danielchsan@gmail.com>
2025-08-16 09:54:13 -0300

    Formatting validation message

---
+++ src/main/java/br/com/danielmonteiro/open_role/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.danielmonteiro.open_role.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+    private String message;
+    private String field;
+}
